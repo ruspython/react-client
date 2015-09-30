14b0a8b25fcf0fa130256312fcd437a747b80071
Vladislav <ruspython@gmail.com>
2015-09-30 20:57:38 +0300

    Gulp configurations

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,7 +1,17 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
 var config = require('../config').markup;
 
-gulp.task('markup', function() {
-  return gulp.src(config.src)
-    .pipe(gulp.dest(config.dest));
+gulp.task('markup', ['less'], function () {
+    return gulp
+        .src(config.src)
+        .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('less', function () {
+    console.log(config.src+'/*.less')
+    return gulp
+        .src('./src/www/less/main.less')
+        .pipe(less())
+        .pipe(gulp.dest('./build/css'));
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,12 +1,17 @@
-
 /* Notes:
-   - gulp/tasks/browserify.js handles js recompiling with watchify
-   - gulp/tasks/browserSync.js watches and reloads compiled files
-*/
+ - gulp/tasks/browserify.js handles js recompiling with watchify
+ - gulp/tasks/browserSync.js watches and reloads compiled files
+ */
 
-var gulp   = require('gulp');
+var gulp = require('gulp');
 var config = require('../config');
 
-gulp.task('watch', ['setWatch', 'browserSync'], function() {
-  gulp.watch(config.markup.src, ['markup']);
+gulp.task('watch', ['setWatch', 'browserSync'], function () {
+    gulp.watch([config.markup.src, "!"+config.markup.src+'/less/*.less'], ['markup']);
+});
+
+gulp.task('less-watcher', function () {
+    console.log('in wathcer')
+    console.log(config.markup.src+'/*.less')
+    gulp.watch(config.markup.src+'/**/*.less', ['less']);
 });
